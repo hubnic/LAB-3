0a062a31f5c74f43dff9cdc4fa5c0b12e121e744
Nicolas <nicolas.hubert.1@etsmtl.net>
2015-03-05 14:24:34 -0500

    test synch

--- data/AretesIn.java
+++ data/AretesIn.java
@@ -3,13 +3,13 @@ package data;
 
 
 public class AretesIn {
-	public static Aretes[] AretesIn;
+	public Aretes[] AretesIn;
 	
 	public void setLength(int length){
 		AretesIn = new Aretes[length];
 	}
 	
-	public static void addAreteIn(Aretes addAretes){
+	public void addAreteIn(Aretes addAretes){
 		for (int i=0; i<AretesIn.length; i++){
 			if(AretesIn[i]==null){
 				AretesIn[i]=addAretes;
