90815c4a577861849b97e44002487db1d5382dc5
marinejt <jean.theo.vinet@gmail.com>
2015-03-05 14:43:35 -0500

    Fix bug pour le static

--- gti310/tp3/parser/ConcreteParser.java
+++ gti310/tp3/parser/ConcreteParser.java
@@ -4,13 +4,13 @@ import data.Aretes;
 import data.AretesIn;
 
 public class ConcreteParser implements Parser{
-
+	private AretesIn aretesIn = new AretesIn();
 	public Object parse(String filename) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 	public void addAretes(int destination, int parent, int poids){
-		AretesIn.addAreteIn(new Aretes(destination, parent, poids));
+		aretesIn.addAreteIn(new Aretes(destination, parent, poids));
 	}
 
 }
