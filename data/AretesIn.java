4ab7c59e59a509b71bd5e480901e9d0cb79b6548
Nicolas <nicolas.hubert.1@etsmtl.net>
2015-03-05 14:10:59 -0500

    changement tache 1,4,2

--- data/AretesIn.java
+++ data/AretesIn.java
@@ -3,14 +3,19 @@ package data;
 
 
 public class AretesIn {
-	Aretes[] AretesIn;
+	public static Aretes[] AretesIn;
 	
 	public void setLength(int length){
 		AretesIn = new Aretes[length];
 	}
 	
-	public void addAreteIn(Aretes addAretes){
-		
+	public static void addAreteIn(Aretes addAretes){
+		for (int i=0; i<AretesIn.length; i++){
+			if(AretesIn[i]==null){
+				AretesIn[i]=addAretes;
+				break;
+			}
+		}
 	}
 	
 }
--- gti310/tp3/parser/ConcreteParser.java
+++ gti310/tp3/parser/ConcreteParser.java
@@ -1,10 +1,16 @@
 package gti310.tp3.parser;
 
+import data.Aretes;
+import data.AretesIn;
+
 public class ConcreteParser implements Parser{
 
 	public Object parse(String filename) {
 		// TODO Auto-generated method stub
 		return null;
+	}
+	public void addAretes(int destination, int parent, int poids){
+		AretesIn.addAreteIn(new Aretes(destination, parent, poids));
 	}
 
 }
